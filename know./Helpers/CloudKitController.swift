780f62888a95f185c58de596831117fe74dac286
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-08-20 16:03:58 -0600

    Added Zone and Share functions to CloudKit Controller

--- know./Helpers/CloudKitController.swift
+++ know./Helpers/CloudKitController.swift
@@ -19,6 +19,22 @@ class CloudKitController {
     let sharedDB = CKContainer.default().sharedCloudDatabase
     var knowZone: CKRecordZone?
     
+    init() {
+        fetchZone(withName: "ShareCycle") { (success) in
+            if success {
+                print("Record Zone Found")
+            } else {
+                self.createRecordZone(zoneName: "ShareCycle", completion: { (success) in
+                    if success {
+                        print("Successfully Created Record Zone")
+                    } else {
+                        print("Failed to Create Record Zone")
+                    }
+                })
+            }
+        }
+    }
+    
     //Save a Record
     func save(record: CKRecord, completion: @escaping (CKRecord?) -> Void) {
         privateDB.save(record) { (record, error) in
@@ -57,6 +73,7 @@ class CloudKitController {
         operation.queuePriority = .high
         operation.completionBlock = {
             completion(true)
+            print("Updated Record in CloudKit")
         }
        privateDB.add(operation)
     }
@@ -69,6 +86,93 @@ class CloudKitController {
                 completion(false)
             }
             completion(true)
+            print("Deleted Record from CloudKit")
+        }
+    }
+    
+    //Create a new CKRecordZone for sharing
+    func createRecordZone(zoneName: String, completion: @escaping (Bool) -> Void) {
+        let zone = CKRecordZone(zoneID: CKRecordZone.ID(zoneName: zoneName))
+        let operation = CKModifyRecordZonesOperation(recordZonesToSave: [zone], recordZoneIDsToDelete: nil)
+        operation.modifyRecordZonesCompletionBlock = { savedZone, _, error in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) \n---\n \(error)")
+                completion(false)
+                return
+        }
+            if savedZone != nil, zone == savedZone?.first {
+                self.knowZone = savedZone?.first
+                completion(true)
+            }
+        }
+        privateDB.add(operation)
+    }
+    
+    //Retrieve Zone if it exists
+    func fetchZone(withName name: String, completion: @escaping (Bool) -> Void) {
+        privateDB.fetch(withRecordZoneID: CKRecordZone.ID(zoneName: name)) { (foundZone, error) in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) \n---\n \(error)")
+                completion(false)
+                return
+            }
+            if let foundZone = foundZone {
+                self.knowZone = foundZone
+                completion(true)
+            }
+        }
+    }
+    
+    //Create a Share
+    public func share(record: CKRecord, type: String, completion: @escaping (CKShare?, CKContainer?, Error?) -> Void) {
+        let shareRecordID = CKRecord.ID(recordName: record.recordID.recordName, zoneID: knowZone!.zoneID)
+        let recordToShare = CKRecord(recordType: type, recordID: shareRecordID)
+        let share = CKShare(rootRecord: recordToShare)
+        share[CKShare.SystemFieldKey.title] = "\(record.recordID)" as CKRecordValue?
+        share[CKShare.SystemFieldKey.shareType] = "com.DMA.know-" as CKRecordValue?
+        let operation = CKModifyRecordsOperation(recordsToSave: [share, recordToShare], recordIDsToDelete: nil)
+        operation.savePolicy = .changedKeys
+        operation.qualityOfService = .userInitiated
+        operation.perRecordCompletionBlock = { record, error in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) \n---\n \(error)")
+                print("Failed Per Record Completion")
+                completion(nil, nil, error)
+            }
+        }
+        operation.modifyRecordsCompletionBlock = { (_, _, error) in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) \n---\n \(error)")
+                print("Failed Modify Completion")
+                completion(nil, nil, error)
+            } else {
+                completion(share, CKContainer.default(), nil)
+            }
+        }
+        privateDB.add(operation)
+    }
+    
+    //Fetch a Share
+    public func fetchShare(metadata: CKShare.Metadata, completion: @escaping ([CKRecord]?) -> Void) {
+        let operation = CKFetchRecordsOperation(recordIDs: [metadata.rootRecordID])
+        operation.perRecordCompletionBlock = { record, _, error in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) \n---\n \(error)")
+                completion(nil)
+                return
+            }
+        }
+        operation.fetchRecordsCompletionBlock = { recordDictionary, error in
+            if let error = error {
+                print("Error in \(#function): \(error.localizedDescription) \n---\n \(error)")
+                completion(nil)
+                return
+            }
+            if let records = recordDictionary {
+                let ckRecords = records.compactMap {$0.value}
+                completion(ckRecords)
+            }
         }
     }
 }
+

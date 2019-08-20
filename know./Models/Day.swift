4c9f5419a663d59eb78c711175e9ad80bf2a9d9c
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-08-20 09:53:35 -0600

    Fixed Day Model Initializers

--- know./Models/Day.swift
+++ know./Models/Day.swift
@@ -13,14 +13,14 @@ class Day {
     
     //Class Objects
     var date: Date
-    var flowDetails: Flow?
-    var symptomList: [Symptom]?
-    var moodList: [Mood]?
-    var sexDetails: [Sex]?
+    var flowDetails: [Flow]
+    var symptomList: [Symptom]
+    var moodList: [Mood]
+    var sexDetails: [Sex]
     var ckRecordID: CKRecord.ID
     
     //Designated Initializer
-    init(date: Date, flowDetails: Flow, symptomList: [Symptom], moodList: [Mood], sexDetails: [Sex], ckRecordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
+    init(date: Date, flowDetails: [Flow] = [], symptomList: [Symptom] = [], moodList: [Mood] = [], sexDetails: [Sex] = [], ckRecordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
         self.date = date
         self.flowDetails = flowDetails
         self.symptomList = symptomList
@@ -31,13 +31,9 @@ class Day {
     
     //Initialize class object from a record
     convenience init?(record: CKRecord) {
-        guard let date = record[DayConstants.dateKey] as? Date,
-        let flowDetails = record[DayConstants.flowKey] as? Flow,
-        let symptomList = record[DayConstants.symptomKey] as? [Symptom],
-        let moodList = record[DayConstants.moodKey] as? [Mood],
-        let sexDetails = record[DayConstants.sexKey] as? [Sex]
+        guard let date = record[DayConstants.dateKey] as? Date
             else { return nil }
-        self.init(date: date, flowDetails: flowDetails, symptomList: symptomList, moodList: moodList, sexDetails: sexDetails, ckRecordID: record.recordID)
+        self.init(date: date, ckRecordID: record.recordID)
     }
 }
 
@@ -46,10 +42,6 @@ extension CKRecord {
     convenience init(day: Day) {
         self.init(recordType: DayConstants.dayTypeKey, recordID: day.ckRecordID)
         self.setValue(day.date, forKey: DayConstants.dateKey)
-        self.setValue(day.flowDetails, forKey: DayConstants.flowKey)
-        self.setValue(day.symptomList, forKey: DayConstants.symptomKey)
-        self.setValue(day.moodList, forKey: DayConstants.moodKey)
-        self.setValue(day.sexDetails, forKey: DayConstants.sexKey)
     }
 }
 
